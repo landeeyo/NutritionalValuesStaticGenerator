83f4a9429829908252e1004903757dff4fc3cb22
landeeyo <landeeyo@10g.pl>
2016-03-26 18:46:21 +0100

    Solution created, data csv file added, simple yet ugly csv parser implemented.

---
+++ NutritionalValuesStaticGenerator/Data/NutritionalDataItem.cs
@@ -0,0 +1,19 @@
+﻿namespace NutritionalValuesStaticGenerator.Data
+{
+    public class NutritionalDataItem
+    {
+        public string Name { get; set; }
+
+        public double Calories { get; set; }
+
+        public double? Protein { get; set; }
+
+        public double? Carbohydrate { get; set; }
+
+        public double? Fiber { get; set; }
+
+        public double? Sugar { get; set; }
+
+        public double? Fat { get; set; }
+    }
+}
---
+++ NutritionalValuesStaticGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using NutritionalValuesStaticGenerator.Logic;
+using System;
+using System.IO;
+
+namespace NutritionalValuesStaticGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", "NutritionalData.csv");
+            DataParser parser = new DataParser(path);
+        }
+    }
+}
